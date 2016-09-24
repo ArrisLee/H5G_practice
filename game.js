0602086e8c816ea42712e9d35d9b9ba45320f041
ArrisLee <arris_li@hotmail.com>
2016-09-24 17:06:28 +1200

    update

--- game.js
+++ game.js
@@ -1,12 +1,16 @@
 var canvas = document.getElementById('game');
 var context = canvas.getContext('2d');
 
-context.font = '50px Impact';
+//set style and location for the text
 context.fillStyle = "#0099CC";
 context.textAlign = 'center';
+
+//set font
+context.font = '50px Impact';
+//set first draw content and the location on canvas
 context.fillText('My First Game', canvas.width/2, canvas.height/2);
 
+
+//second draw -- click text
 context.font = '20px Courier Bold';
-context.fillStyle = "#0099CC";
-//context.textAlign = 'center';
 context.fillText('Click to start', canvas.width/2, canvas.height/1.5);
