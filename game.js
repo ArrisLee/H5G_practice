fe14bf29511542dcfad4b4d038259b2980f0f9ed
ArrisLee <arris_li@hotmail.com>
2016-09-30 00:29:10 +1300

    update

--- game.js
+++ game.js
@@ -5,9 +5,11 @@ var gameStarted = false;
 //key mapping
 var keys = [];
 //speed down -- higher means more slippery
-var friction = 0.84;
+var friction = 0.8;
+//limit jump height
+var gravity = 0.98;
 var player = {
-    //position on x axis on canvas
+    //position on x axis on canvas, topleft of the canvas is 0,0
     x: 5,
     //as the player's height is 20px
     y: canvas.height - 20,
@@ -17,11 +19,17 @@ var player = {
     velX: 0,
     velY: 0,
     color: "#ff0000",
+    jumpStrength: 14,
+    jumping: false,
     draw: function() {
         context.fillStyle = this.color;
         context.fillRect(this.x, this.y, this.width, this.height);
     }
 }
+
+var platforms = [];
+var platform_width = 120;
+var platform_height = 10;
 
 
 //logic
@@ -51,7 +59,7 @@ function intro_screen() {
 
 
     //second draw -- click text
-    context.font = '16px Courier New';
+    context.font = '18px Courier New';
     context.fillText('Press ENTER to start', canvas.width / 2, canvas.height / 1.5);
 }
 
@@ -69,20 +77,41 @@ function update() {
     //console.log("Game running");
     //clearMap();
     player.draw();
+
+    //move right
     if (keys[39]) {
         if (player.velX < player.speed) {
             player.velX++;
         }
     }
+
+    //move left
     if (keys[37]) {
         if (player.velX > -player.speed) {
             player.velX--;
         }
     }
+
+    //move up
+    if (keys[38] || keys[32]) {
+        if (!player.jumping) {
+            player.velY = -player.jumpStrength;
+            player.jumping = true;
+        }
+    }
     //basic movement draw
     player.x += player.velX;
+    player.y += player.velY;
+
     //speed down by timing friction
     player.velX *= friction;
+    player.velY += gravity;
+
+    //set a condition to stop player from dropping
+    if (player.y >= canvas.height - player.height) {
+        player.y = canvas.height - player.height;
+        player.jumping = false;
+    }
 }
 
 function clearMap() {
